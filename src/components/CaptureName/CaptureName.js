96d5bfdd060aabcbb931b48f23766fc8da123986
Eric Willroth <ewillroth@gmail.com>
2019-02-19 09:44:42 -0600

    fix typo

--- src/components/CaptureName/CaptureName.js
+++ src/components/CaptureName/CaptureName.js
@@ -7,7 +7,7 @@ class CaptureName extends Component {
     render() {
         return (
             <div className="CaptureName">
-				<p>""Prepare yourself to embark on a most noble quest. Enter the dungeon and retrieve the Tome of Ultimate Coding Knowledge."		</p>
+				<p>"Prepare yourself to embark on a most noble quest. Enter the dungeon and retrieve the Tome of Ultimate Coding Knowledge."		</p>
 				<img src={wizard} alt="wizard"></img>
                 <h4>What is your name?</h4>
 				<input type="text" 
